5666dbcb1ef344557e08b69b308077240969490d
Sergiu Prodan <sergiuc.prodan@gmail.com>
2021-06-04 14:21:53 +0300

    Initial commit

---
+++ src/main/java/com/conqueror/employeemanager/repo/EmployeeRepo.java
@@ -0,0 +1,12 @@
+package com.conqueror.employeemanager.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.conqueror.employeemanager.model.Employee;
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
+}
